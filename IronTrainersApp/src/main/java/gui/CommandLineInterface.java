0bee2a1f60848dcac4c26613c2c53d7422c96c2f
José Aleixo <josealeixo.pc@gmail.com>
2017-12-31 19:33:26 +0000

    added readme

--- IronTrainersApp/src/main/java/gui/CommandLineInterface.java
+++ IronTrainersApp/src/main/java/gui/CommandLineInterface.java
@@ -1,7 +1,7 @@
 package src.main.java.gui;
 
 public class CommandLineInterface {
-    public static void main(String[] args) {
-      System.out.println("Hellooo"); 
-    }
-  } 
+  public static void main(String[] args) {
+    System.out.println("Hellooo"); 
+  }
+} 

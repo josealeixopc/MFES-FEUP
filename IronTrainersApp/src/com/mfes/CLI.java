6220f256b9658a884dc312d5030df4259c6d0cad
José Aleixo <josealeixo.pc@gmail.com>
2018-01-01 04:05:36 +0000

    Created Menu class

--- IronTrainersApp/src/com/mfes/CLI.java
+++ IronTrainersApp/src/com/mfes/CLI.java
@@ -1,8 +1,54 @@
 package com.mfes;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CLI {
 
+
+
+    private Scanner scanner;
+
+    CLI() {
+        scanner = new Scanner(System.in);
+    }
+
     public void run(){
-        System.out.println("hello");
+        showInitialMenu();
+    }
+
+    private void showAppBanner(){
+        System.out.println("\n" +
+                "*******************************\n" +
+                "*                             *\n" +
+                "*        IRON TRAINERS        *\n" +
+                "*                             *\n" +
+                "*******************************\n");
+    }
+
+    private void showInitialMenu(){
+        showAppBanner();
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add("Log In");
+        options.add("Register");
+
+        Menu loginMenu = new Menu(this.scanner, "Login / Register", options) {
+            @Override
+            void selectedOptionTrigger(int selectedOption) {
+                switch (selectedOption) {
+                    case 1: System.out.println("You have an account"); break;
+                    case 2: System.out.println("You don't have an account"); break;
+                    default:
+                        break;
+                }
+            }
+        };
+
+        loginMenu.show();
+    }
+
+    private void showLoginForm(){
+
     }
 }
---
+++ IronTrainersApp/src/com/mfes/Menu.java
@@ -0,0 +1,76 @@
+package com.mfes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public abstract class Menu {
+
+    Scanner scanner;
+    String menuName;
+    ArrayList<String> options;
+    Menu parent;
+
+    Menu(Scanner scanner, String menuName, ArrayList<String> options){
+        this(scanner, menuName, options, null);
+    }
+
+    Menu(Scanner scanner, String menuName, ArrayList<String> options, Menu parent){
+        this.scanner = scanner;
+        this.menuName = menuName;
+        this.options = options;
+        this.parent = parent;
+    }
+
+    void show(){
+        this.printMenu();
+        int selectedOption = this.getSelectedOption();
+        this.selectedOptionTrigger(selectedOption);
+    }
+
+    abstract void selectedOptionTrigger(int selectedOption);
+
+    private void printMenu(){
+
+        System.out.println("** " + this.menuName + " **\n");
+
+        for(int i = 1; i <= this.options.size(); i++){
+            System.out.println(i + " - " + this.options.get(i-1));
+        }
+
+        System.out.print("\nOption to select: ");
+    }
+
+    private int getSelectedOption(){
+
+        boolean validInput = false;
+        int selectedOption = -1;
+        String input;
+
+        while(!validInput){
+
+            input = this.scanner.nextLine();
+
+            try {
+                selectedOption = Integer.parseInt(input);
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+
+            if(selectedOption >= 1 && selectedOption <= this.options.size()) {
+                validInput = true;
+            }
+            else {
+                printInvalidInputMessage();
+            }
+        }
+
+        return selectedOption;
+    }
+
+    private void printInvalidInputMessage(){
+        System.out.print( "Invalid input!!!\n" +
+                "Please selected a different option: ");
+    }
+
+}
